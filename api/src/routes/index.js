1795a3a14c5f810413603b496e9223b65276fe11
Luis Galea <luisgalea31@gmail.com>
2024-02-19 13:12:20 -0400

    feat<api\src\routes>: create routes backend

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,30 @@
 const { Router } = require('express');
+const getIngredient = require('../controllers/Ingredient/getIngredient');
+const createIngredient = require('../controllers/Ingredient/createIngredient')
+const getProvider = require('../controllers/Provider/getProvider');
+const createProvider = require('../controllers/Provider/createProvider');
+const getMaterialsInventory = require('../controllers/Inventory/getMaterialsInventory');
+const RawMaterialsInventory = require('../controllers/Inventory/rawMaterialsInventory');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
-
 // Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+// rutas de ingredientes
+router.get('/ingredient/get_allingredient', getIngredient)
+router.post('/ingredient/create_ingredient', createIngredient)
+
+// rutas de proveedor
+router.get('/provider/get_allproviders', getProvider)
+router.post('/provider/create_provider', createProvider)
+
+// rutas de inventario
+router.get('/inventory/get_ingredient', getMaterialsInventory)
+router.post('/inventory/create_ingredient', RawMaterialsInventory)
+
+// rutas de receta
+
 
 
 module.exports = router;
