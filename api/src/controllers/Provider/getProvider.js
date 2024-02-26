8be1bee506b674c53b5d535291dec7564d44a61d
Luis Galea <luisgalea31@gmail.com>
2024-02-26 15:10:16 -0400

    feat<api\src\controllers>: add getprovider

--- api/src/controllers/Provider/getProvider.js
+++ api/src/controllers/Provider/getProvider.js
@@ -4,7 +4,7 @@ const response = require('../../utils/response')
 module.exports = async (req, res) => {
     try {        
         const provider = await Proveedor.findAll()
-        response(res, 201, provider)
+        response(res, 200, provider)
     } catch (error) {
         console.error('Error: ', error.message)
     }
