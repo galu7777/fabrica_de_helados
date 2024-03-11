5468430822b904fb04fe7e15cb0ed86cea3ad005
Miguel Velasco <migue16velasco@gmail.com>
2024-03-11 10:17:52 -0400

    feat: add clientes to navbar

--- client/src/components/Navbar/NavBar.jsx
+++ client/src/components/Navbar/NavBar.jsx
@@ -17,7 +17,7 @@ import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
 const drawerWidth = 240;
-const navItems = ['home', 'ingredientes', 'Receta', 'Proveedores', 'Inventario', 'Batido'];
+const navItems = ['home', 'ingredientes', 'Recetas', 'Proveedores', 'Inventario', 'Batidos', 'Clientes'];
 
 function DrawerAppBar(props) {
   const { window } = props;
