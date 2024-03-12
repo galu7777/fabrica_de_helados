4a4f49493852159929f17ed26d4b2c408d215ee1
Miguel Velasco <migue16velasco@gmail.com>
2024-03-12 05:52:08 -0400

    feat: apply stiles login jsx

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -1,21 +1,26 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import Swal from "sweetalert2";
 import { signin } from "../../redux/actions/actions";
+import TextField from "@mui/material/TextField";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import "./Login.style.css";
+import InputAdornment from "@mui/material/InputAdornment";
 
 function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const [error, setError] = useState("");
-  const [currentUser, setCurrentUser] = useState({});
+  // const [error, setError] = useState("");
+  // const [currentUser, setCurrentUser] = useState({});
 
   const [cUser, setUser] = useState({
     email: "",
     password: "",
   });
+  const [showPassword, setShowPassword] = useState(false);
+
 
   // useEffect(() => {
   //   if (usuarioActual) {
@@ -59,55 +64,65 @@ function Login() {
   };
 
   return (
-    <div className="body-login bg-gray-200">
+    <div className="body-login bg-red-400">
       <div className="login-container flex content-center">
-        <div className="login-div bg-gray-300">
-          <h1 className="text-2xl font-bold text-center text-[#9b1028]">Iniciar Sesión</h1>
-          {error && <p>{error}</p>}
+        <div className="container flex h-1/2  justify-center">
+          <div className="login-div bg-white rounded-lg shadow-2xl ">
+            <h1 className="text-2xl font-bold text-center text-[#9b1028]">
+              Iniciar Sesión
+            </h1>
+            {/* {error && <p>{error}</p>} */}
 
-          <form className="w-full" onSubmit={handleSubmit}>
-            <div className="div-inputs">
-              <label htmlFor="email" className="lbl-title">
-                Email
-              </label>
-              <input
-                value={cUser.email}
-                type="text"
-                name="email"
-                id="email"
-                className="form-control"
-                onChange={handleChange}
-                placeholder="youremail@company.com"
-              />
-            </div>
+            <form className="w-full" onSubmit={handleSubmit}>
+              <div className="div-inputs">
+                <TextField
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  value={cUser.email}
+                  onChange={handleChange}
+                  type="text"
+                  name="email"
+                  id="email"
+                />
+              </div>
 
-            <div className="div-inputs">
-              <label htmlFor="password" className="lbl-title">
-                Contraseña
-              </label>
-              <input
-                value={cUser.password}
-                type="password"
-                name="password"
-                id="password"
-                className="form-control"
-                onChange={handleChange}
-                placeholder="*************"
-              />
-            </div>
+              <div className="div-inputs">
+                <TextField
+                  label="password"
+                  variant="outlined"
+                  fullWidth
+                  value={cUser.password}
+                  onChange={handleChange}
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  id="password"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <VisibilityOutlinedIcon
+                          onClick={() => setShowPassword((prev) => !prev)}
+                          className="cursor-pointer text-black"
+                        />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </div>
 
-            <div className="w-full flex justify-center p-3">
-              <button type="submit" className="btn-is">
-                Iniciar sesión
-              </button>
-            </div>
-          </form>
-          <p>
-            ¿No tienes una cuenta?{" "}
-            <Link className="link" to="/register">
-              Regístrate
-            </Link>
-          </p>
+              <div className="w-full flex justify-center p-3">
+                <button type="submit" className="btn-is">
+                  Iniciar sesión
+                </button>
+              </div>
+            </form>
+            <p>
+              ¿No tienes una cuenta?{" "}
+              <Link className="text-blue-500 font-semibold hover:text-indigo-700" to="/register">
+                Regístrate
+              </Link>
+            </p>
+          </div>
         </div>
 
         <div className="login-image-div"></div>
