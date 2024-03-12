d8f89a113bd7d2642532b140ee2ffbe248b1b8e9
Miguel Velasco <migue16velasco@gmail.com>
2024-03-12 09:14:59 -0400

    feat: add typepopsicle app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import Smoothie from "./views/Smoothie/Smoothie";
 import CreateSmoothie from "./views/Smoothie/CreateSmoothie";
 import Customers from "./views/Customer/Customers";
 import CreateCustomers from "./views/Customer/CreateCustomers";
-
+import TypePopsicle from "./views/Popsicle/TypePopsicle/TypePopsicle";
 import Login from "./views/Login/Login";
 import Register from "./views/Register/Register";
 
@@ -21,28 +21,29 @@ function App() {
   const location = useLocation();
 
   return (
-      <div>
-        {
-          location.pathname !== "/register" && location.pathname !== "/login" && <NavBar />
-        }
-        <Routes>
-          <Route path="*" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/ingredientes" element={<Ingredient />} />
-          <Route path="/recetas" element={<Recipe />} />
-          <Route path="/crear_receta" element={<CreateRecipe />} />
-          <Route path="/proveedores" element={<Provider />} />
-          <Route path="/crear_proveedore" element={<CreateProvider />} />
-          <Route path="/Inventario" element={<Inventory />} />
-          <Route path="/crear_inventario" element={<CreateInventory />} />
-          <Route path="/batidos" element={<Smoothie />} />
-          <Route path="/crear_smoothie" element={<CreateSmoothie />} />
-          <Route path="/Clientes" element={<Customers />} />
-          <Route path="/crear_clientes" element={<CreateCustomers />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
+    <div>
+      {location.pathname !== "/register" && location.pathname !== "/login" && (
+        <NavBar />
+      )}
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/ingredientes" element={<Ingredient />} />
+        <Route path="/recetas" element={<Recipe />} />
+        <Route path="/crear_receta" element={<CreateRecipe />} />
+        <Route path="/proveedores" element={<Provider />} />
+        <Route path="/crear_proveedore" element={<CreateProvider />} />
+        <Route path="/Inventario" element={<Inventory />} />
+        <Route path="/crear_inventario" element={<CreateInventory />} />
+        <Route path="/batidos" element={<Smoothie />} />
+        <Route path="/crear_smoothie" element={<CreateSmoothie />} />
+        <Route path="/Clientes" element={<Customers />} />
+        <Route path="/crear_clientes" element={<CreateCustomers />} />
+        <Route path="/TipoPaletas" element={<TypePopsicle />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
   );
 }
 
