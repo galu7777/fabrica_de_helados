e200716adc473cefc076cc1df6fe7075ecf17086
Luis Galea <luisgalea31@gmail.com>
2024-03-13 18:39:36 -0400

    fix<api\src\mopdels\recipe.js>: fix properties

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -14,6 +14,7 @@ module.exports = (sequelize) => {
             },
             nombre: {
                 type: DataTypes.STRING,
+                unique: true
             }
         },
         {
