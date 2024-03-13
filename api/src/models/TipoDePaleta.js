31860644f048b9ee83d7a925dbd79adb0f5596a1
Luis Galea <luisgalea31@gmail.com>
2024-03-13 18:40:56 -0400

    fix<api\src\models\tipodepaleta.js>: fix properties

--- api/src/models/TipoDePaleta.js
+++ api/src/models/TipoDePaleta.js
@@ -13,6 +13,7 @@ module.exports = (sequelize) => {
             },
             nombre: {
                 type: DataTypes.STRING,
+                unique: true
             },
         },
         {
