bb274f570be640135853ddb2d4376f24bdc36747
Luis Galea <luisgalea31@gmail.com>
2024-03-13 18:55:40 -0400

    feat<api\src\controllers\sale\createsale.js> add validation response 500

--- api/src/controllers/Sale/createSale.js
+++ api/src/controllers/Sale/createSale.js
@@ -4,18 +4,18 @@ const response = require('../../utils/response')
 module.exports = async (req, res) => {
     const { id_cliente, id_inventario_paleta, cantidad, precio, monto, tasa  } = req.body;
     try {
-        const foundSmoothie = await Cliente.findOne({
+        const foundClient = await Cliente.findOne({
             where: { id_cliente }
         })
 
-        if(foundSmoothie){
+        if(foundClient){
             const foundPopsicle = await InventarioPaleta.findOne({
                 where: { id_inventario_paleta }
             })
 
             await InventarioPaleta.create({
                 nombre_paleta: foundPopsicle.nombre_paleta,
-                tipo_paleta: foundPopsicle.paleta,
+                tipo_paleta: foundPopsicle.tipo_paleta,
                 cantidad: -cantidad,
                 peso_unitario: foundPopsicle.peso_unitario,
                 unidad_medida: "GRS",
@@ -37,7 +37,7 @@ module.exports = async (req, res) => {
         return response(res, 200, "The client is not registered in the database.")
     } catch (error) {
         console.error('Error: ', error.message)
-        return response(res, 500, 'Internal Server Error')
+        return response(res, 500, `Internal Server Error: ${error.message}`)
     }
 
 }
