5b89880b757a65ea9f27e455315e80694e309dbe
Luis Galea <luisgalea31@gmail.com>
2024-03-14 00:19:37 -0400

    feat<api\src\controllers\inventory\rawmaterialsinventory.js>: add unidad_medida

--- api/src/controllers/Inventory/rawMaterialsInventory.js
+++ api/src/controllers/Inventory/rawMaterialsInventory.js
@@ -2,10 +2,10 @@ const { InventarioMateriaPrima } = require('../../db')
 const response = require('../../utils/response')
 
 module.exports = async (req, res) => {
-    const { cantidad, tipo, IngredienteId, ProveedorId } = req.body;
+    const { cantidad, unidad_medida, tipo, IngredienteId, ProveedorId } = req.body;
     try {        
         const entry = await InventarioMateriaPrima.create({
-            cantidad, tipo, IngredienteId, ProveedorId
+            cantidad, unidad_medida, tipo, IngredienteId, ProveedorId
         })
         return response(res, 201, {message: 'success', entry})
     } catch (error) {
