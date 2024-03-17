14469919b4f72e097c3bd5f6bae23cda8be7900d
Miguel Velasco <migue16velasco@gmail.com>
2024-03-16 23:20:07 -0400

    feat: remove popsicle type

--- api/src/controllers/Popsicle/InventoryPopsicle/createInventoryPopsicle.js
+++ api/src/controllers/Popsicle/InventoryPopsicle/createInventoryPopsicle.js
@@ -1,44 +1,45 @@
-const { InventarioPaleta, Paleta, TipoDePaleta, BatidaDeHelado } = require('../../../db')
-const response = require('../../../utils/response')
+const { InventarioPaleta, Paleta, TipoDePaleta, BatidaDeHelado } = require('../../../db');
+const response = require('../../../utils/response');
 
 module.exports = async (req, res) => {
-    const { id_batida, id_tipo_de_paleta, id_paleta} = req.body;
+    const { id_batida, id_paleta} = req.body;
     try {
         const foundSmoothie = await InventarioPaleta.findOne({
             where: { BatidaDeHeladoId: id_batida }
-        })
+        });
 
         if(foundSmoothie){
-            return response(res, 200, "Shake already registered in the db.")
+            return response(res, 200, "Shake already registered in the db.");
         }
-        const foundedSmoothie = await BatidaDeHelado.findByPk(id_batida)
-        const foundedPopsicleType = await TipoDePaleta.findByPk(id_tipo_de_paleta)
-        const foundedPopsicle = await Paleta.findByPk(id_paleta)
 
-        const typePopsicle = foundedPopsicleType.dataValues
-        const smoothie = foundedSmoothie.dataValues
-        const popsicle = foundedPopsicle.dataValues
-        
-        if(typePopsicle && smoothie && popsicle){
-            const { id, cantidad } = smoothie
-            const cantidad_paleta = Math.floor(cantidad * 1000 / popsicle.peso)
+        const foundedSmoothie = await BatidaDeHelado.findByPk(id_batida);
+        const foundedPopsicle = await Paleta.findByPk(id_paleta);
+
+        const smoothie = foundedSmoothie.dataValues;
+        const popsicle = foundedPopsicle.dataValues;
+
+        if( smoothie && popsicle){
+            const { id, cantidad } = smoothie;
+            const cantidad_paleta = Math.floor(cantidad * 1000 / popsicle.peso);
+
+            // Obtener el tipo de paleta asociado a la paleta
+
 
             const entry_inventory_popsicle = await InventarioPaleta.create({
                 nombre_paleta: popsicle.nombre,
-                tipo_paleta: typePopsicle.nombre,
                 cantidad: cantidad_paleta,
                 peso_unitario: popsicle.peso,
                 unidad_medida: "GRS",
                 tipo: "ENTREGA",
                 BatidaDeHeladoId: id,
-                TipoDePaletumId: typePopsicle.id
-            })
-            return response(res, 201, {entry_inventory_popsicle})
+                TipoDePaletumId: popsicle.tipo_paleta,
+            });
+            return response(res, 201, {entry_inventory_popsicle});
         } else {
-            return response(res, 500, "Don't founded info")
+            return response(res, 500, "Don't founded info");
         }
     } catch (error) {
-        console.error('Error: ', error.message)
-        return response(res, 500, `Internal Server Error: ${error.message}`)
+        console.error('Error: ', error.message);
+        return response(res, 500, `Internal Server Error: ${error.message}`);
     }
-}
+};
