99648cfe8bdc532890fe95ced7d6d245533cb3b4
Miguel Velasco <migue16velasco@gmail.com>
2024-03-16 23:20:58 -0400

    feat: add popsicles type

--- api/src/controllers/Popsicle/NewPopsicle/createNewPopsicle.js
+++ api/src/controllers/Popsicle/NewPopsicle/createNewPopsicle.js
@@ -1,17 +1,25 @@
-const { Paleta } = require('../../../db')
-const response = require('../../../utils/response')
+const { Paleta, TipoDePaleta } = require('../../../db');
+const response = require('../../../utils/response');
 
 module.exports = async (req, res) => {
-    const { nombre, peso, descripcion } = req.body;    
-    try {        
-        const newPaleta = await Paleta.create({
-            nombre,
-            peso,
-            descripcion
-        })
-        response(res, 201, newPaleta)
+    const { nombre, peso, descripcion, id_tipo_de_paleta } = req.body;
+    try {
+        const foundedPopsicleType = await TipoDePaleta.findByPk(id_tipo_de_paleta);
+        if (foundedPopsicleType !== null) {
+            const typePopsicle = foundedPopsicleType.dataValues;
+            console.log(typePopsicle.id)
+            const newPaleta = await Paleta.create({
+                nombre,
+                peso,
+                descripcion,
+                tipo_paleta: typePopsicle.id
+            });
+            return response(res, 201, newPaleta);
+        } else {
+            return response(res, 200, "The Popsicles' Type is not registered in the database.");
+        }
     } catch (error) {
-        console.error('Error: ', error.message)
-        return response(res, 500, `Internal Server Error: ${error.message}`)
+        console.error('Error: ', error.message);
+        return response(res, 500, `Internal Server Error: ${error.message}`);
     }
-}
+};
