009a6fa158e22bf36cf17894cb37e1741acb25d3
Miguel Velasco <migue16velasco@gmail.com>
2024-03-18 06:46:26 -0400

    feat: add TipoDePaleta to getPopsicle

--- api/src/controllers/Popsicle/NewPopsicle/getNewPopsicle.js
+++ api/src/controllers/Popsicle/NewPopsicle/getNewPopsicle.js
@@ -1,9 +1,16 @@
-const { Paleta } = require('../../../db')
+const { Paleta, TipoDePaleta } = require('../../../db')
 const response = require('../../../utils/response')
 
-module.exports = async (req, res) => { 
-    try {        
-        const paleta = await Paleta.findAll()
+module.exports = async (req, res) => {
+    try {
+        const paleta = await Paleta.findAll({
+            include: [
+                {
+                    model: TipoDePaleta,
+                    attributes: ['id', 'nombre'],
+                }
+            ]
+        })
         response(res, 201, paleta)
     } catch (error) {
         console.error('Error: ', error.message)
