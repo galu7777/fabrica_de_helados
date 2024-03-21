2729c94277b61902fe26bdbf618fe78a4370c680
Luis Galea <luisgalea31@gmail.com>
2024-03-21 12:17:16 -0400

    feat<api\src\controllers\inventory\rawmaterialsinventory.js>: registered stock materials inventory

--- api/src/controllers/Inventory/rawMaterialsInventory.js
+++ api/src/controllers/Inventory/rawMaterialsInventory.js
@@ -1,13 +1,48 @@
-const { InventarioMateriaPrima } = require('../../db')
+const { InventarioMateriaPrima, StockMateriaPrima } = require('../../db')
 const response = require('../../utils/response')
 
 module.exports = async (req, res) => {
-    const { cantidad, tipo, IngredienteId, ProveedorId } = req.body;
-    try {        
-        const entry = await InventarioMateriaPrima.create({
-            cantidad, tipo, IngredienteId, ProveedorId
+    const { cantidad, unidad_medida, tipo, IngredienteId, ProveedorId } = req.body;
+    try {    
+        if(cantidad == 0 || cantidad <= 0){
+            return response(res, 500, {message: 'quantity must be a positive number.'})
+        }
+        const foundIngredient = await StockMateriaPrima.findOne({
+            where: {IngredienteId}
         })
-        return response(res, 201, {message: 'success', entry})
+        if(foundIngredient && tipo === "SALIDA") {
+            if(foundIngredient.cantidad - cantidad <= 0 ){
+                return response(res, 400, {message: 'insufficient quantity in inventory.'})
+            } else {
+                cnt = foundIngredient.cantidad - cantidad               
+                const entry = await foundIngredient.update({
+                    cantidad: cnt
+                })
+                await InventarioMateriaPrima.create({
+                    cantidad: -cantidad, unidad_medida, tipo, IngredienteId, ProveedorId
+                })
+                return response(res, 201, {message: 'success', entry})
+            }
+        }
+        else if(foundIngredient && tipo === "ENTREGA"){
+            cnt = foundIngredient.cantidad + cantidad           
+            const entry = await foundIngredient.update({
+                cantidad: cnt
+            })
+            await InventarioMateriaPrima.create({
+                cantidad, unidad_medida, tipo, IngredienteId, ProveedorId
+            })
+            return response(res, 201, {message: 'success', entry})
+        }
+        else {
+            const entry = await StockMateriaPrima.create({
+                cantidad, unidad_medida, tipo, IngredienteId, ProveedorId 
+            })
+            await InventarioMateriaPrima.create({
+                cantidad, unidad_medida, tipo, IngredienteId, ProveedorId
+            })
+            return response(res, 201, {message: 'success', entry})
+        }
     } catch (error) {
         console.error('Error: ', error.message)
         return response(res, 500, `Internal Server Error: ${error.message}`)
