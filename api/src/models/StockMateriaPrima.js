9287e57e3273cd4c07f697de99a5f0a1bc424789
Luis Galea <luisgalea31@gmail.com>
2024-03-22 08:59:53 -0400

    feat<api\src\models\stockmateriaprima.js>: create and config of models

---
+++ api/src/models/StockMateriaPrima.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define(
+        "StockMateriaPrima",
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                allowNull: false,
+                autoIncrement: true,
+                field: "id_stock_materia",
+            },
+            cantidad: {
+                type: DataTypes.FLOAT
+            },
+            unidad_medida: {
+                type: DataTypes.ENUM("KG", "GR", "L", "ML", "OZ", "UND"),
+                defaultValue: 'KG'
+            },
+            tipo: {
+                type: DataTypes.ENUM('ENTREGA','SALIDA'),
+            }
+        },
+        {
+            timestamps: true,
+        }
+    );
+};
