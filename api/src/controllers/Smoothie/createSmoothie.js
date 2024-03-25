51e0cbbc065b2fef7e06124cab83d3ebb9b854af
Luis Galea <luisgalea31@gmail.com>
2024-03-25 12:39:12 -0400

    feat<api\src\controllers\smoothie\createsmoothie.js>: add validations and func convertund

--- api/src/controllers/Smoothie/createSmoothie.js
+++ api/src/controllers/Smoothie/createSmoothie.js
@@ -1,6 +1,21 @@
 const { Recipe, Ingrediente, RecipeIngrediente, BatidaDeHelado, InventarioMateriaPrima, StockMateriaPrima } = require('../../db');
 const response = require('../../utils/response');
 
+// Función para convertir unidades a gramos (para peso) o mililitros (para volumen)
+function convertirUnidades(cantidad, unidad_medida) {
+    // Definir factores de conversión
+    const conversion = {
+        'KG': 1000,
+        'GRS': 1,
+        'L': 1000,
+        'ML': 1,
+        'OZ': 28.35 // Onza a gramos
+    };
+
+    // Convertir la cantidad a gramos o mililitros
+    return cantidad * conversion[unidad_medida];
+}
+
 module.exports = async (req, res) => {
     const { id_receta } = req.body;
     try {
@@ -23,28 +38,36 @@ module.exports = async (req, res) => {
 
         // Verificar si hay suficiente stock para todos los ingredientes
         for (const ingrediente of foundRecipe.Ingredientes) {
+            const cantidadEnGramos = convertirUnidades(ingrediente.RecipeIngrediente.cantidad, ingrediente.RecipeIngrediente.unidad_medida);
             const stock = await StockMateriaPrima.findOne({
                 where: { IngredienteId: ingrediente.id }
             });
 
-            if (!stock || stock.cantidad < ingrediente.RecipeIngrediente.cantidad) {
+            const converCantidad = convertirUnidades(stock.cantidad, stock.unidad_medida);
+
+            if (!stock || converCantidad < cantidadEnGramos) {
                 return response(res, 400, `No hay suficiente stock para el ingrediente ${ingrediente.nombre}`);
             }
         }
 
         // Si hay suficiente stock para todos los ingredientes, proceder con las actualizaciones
         for (const ingrediente of foundRecipe.Ingredientes) {
+            const cantidadEnGramos = convertirUnidades(ingrediente.RecipeIngrediente.cantidad, ingrediente.RecipeIngrediente.unidad_medida);
             const stock = await StockMateriaPrima.findOne({
                 where: { IngredienteId: ingrediente.id }
             });
 
-            const cnt = stock.cantidad - ingrediente.RecipeIngrediente.cantidad;
+            const stockConvertTotal = convertirUnidades(stock.cantidad, stock.unidad_medida)
+            const cnt = stockConvertTotal - cantidadEnGramos;
+            const amountToRecord = cnt / 1000
+
             await stock.update({
-                cantidad: cnt
+                cantidad: amountToRecord
             });
             await InventarioMateriaPrima.create({
                 IngredienteId: ingrediente.id,
                 cantidad: -ingrediente.RecipeIngrediente.cantidad,
+                unidad_medida: ingrediente.RecipeIngrediente.unidad_medida,
                 tipo: 'SALIDA',
                 ProveedorId: 1,
             });
@@ -53,12 +76,12 @@ module.exports = async (req, res) => {
         // Crear la BatidaDeHelado después de registrar las salidas de inventario
         const newBatidaDeHelado = await BatidaDeHelado.create({
             id_recipe: id_receta,
-            cantidad: foundRecipe.Ingredientes.reduce((acc, item) => acc + item.RecipeIngrediente.cantidad, 0),
+            cantidad: foundRecipe.Ingredientes.reduce((acc, item) => acc + convertirUnidades(item.RecipeIngrediente.cantidad, item.RecipeIngrediente.unidad_medida), 0),
         });
 
-        response(res, 201, { message: `Batida de helado creada con éxito. Salidas registradas.`, newBatidaDeHelado });
+        return response(res, 201, { message: `Batida de helado creada con éxito. Salidas registradas.`, newBatidaDeHelado });
     } catch (error) {
         console.error('Error: ', error.message);
-        response(res, 500, `Internal Server Error: ${error.message}`);
+        return response(res, 500, `Internal Server Error: ${error.message}`);
     }
 };
