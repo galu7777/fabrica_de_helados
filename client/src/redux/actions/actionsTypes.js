bf56a807dc09cd040650d13c4a00fa087e8b026d
Miguel Velasco <migue16velasco@gmail.com>
2024-03-25 11:55:38 -0400

    feat: export stock mp

--- client/src/redux/actions/actionsTypes.js
+++ client/src/redux/actions/actionsTypes.js
@@ -29,3 +29,6 @@ export const REGISTER = 'REGISTER'
 export const SIGNIN = 'SIGNIN'
 export const GET_SALES = 'GET_SALES'
 export const ADD__SALE = 'ADD__SALE'
+export const GET_STOCK_MP = 'GET_STOCK_MP'
+
+
