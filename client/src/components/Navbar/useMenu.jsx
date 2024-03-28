28e6c0a49291f50fec137c252840c09f4a2a91bc
Miguel Velasco <migue16velasco@gmail.com>
2024-03-27 21:09:13 -0400

    feat: useMenu components

---
+++ client/src/components/Navbar/useMenu.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const useMenu = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+  return { anchorEl, open, handleClick, handleClose };
+};
+
+export default useMenu;
