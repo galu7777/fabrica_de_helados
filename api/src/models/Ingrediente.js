14a483019ef6d7709a502f02edcd0c821e501da7
Luis Galea <luisgalea31@gmail.com>
2024-03-29 13:52:44 -0400

    feat<api\src\models\ingrediente.js>: add unidad_medida

--- api/src/models/Ingrediente.js
+++ api/src/models/Ingrediente.js
@@ -14,7 +14,12 @@ module.exports = (sequelize) => {
             nombre: {
                 type: DataTypes.STRING,
                 unique: true
-            }
+            },
+            unidad_medida: {
+                type: DataTypes.ENUM("KG", "GRS", "L", "ML", "OZ", "UND"),
+                defaultValue: 'KG'
+            },
+
         },
         {
             timestamps: true,
