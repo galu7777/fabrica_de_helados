b8cbf5c4ba7b7b1afd323de6542c7cf02c40dbd2
Luis Galea <luisgalea31@gmail.com>
2024-03-29 13:11:36 -0400

    feat<api\src\controllers\popsicle\newpopsicle\createnewpopsicle.js>add upload image

--- api/src/controllers/Popsicle/NewPopsicle/createNewPopsicle.js
+++ api/src/controllers/Popsicle/NewPopsicle/createNewPopsicle.js
@@ -1,19 +1,26 @@
-const { Paleta } = require('../../../db')
-const response = require('../../../utils/response')
+const { Paleta } = require('../../../db');
+const response = require('../../../utils/response');
 
 module.exports = async (req, res) => {
-    const { nombre, peso, precio,  descripcion, id_tipo_paleta } = req.body;
+    const { nombre, peso, precio, descripcion, id_tipo_paleta } = req.body;    
     try {
+        if (!req.file) {
+            return response(res, 400, "No se proporcionó ningún archivo de imagen");
+        }
+        const image = req.file.buffer.toString('base64');
+
         const newPaleta = await Paleta.create({
             nombre,
+            image,
             peso,
             precio,
             descripcion,
             TipoDePaletumId: id_tipo_paleta
-        })
-        response(res, 201, newPaleta)
+        });
+
+        response(res, 201, newPaleta);
     } catch (error) {
-        console.error('Error: ', error.message)
-        return response(res, 500, `Internal Server Error: ${error.message}`)
+        console.error('Error: ', error.message);
+        return response(res, 500, `Internal Server Error: ${error.message}`);
     }
 }
