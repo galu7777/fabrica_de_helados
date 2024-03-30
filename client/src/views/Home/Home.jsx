b9bab2b35268028142e9e4084ca78b68070ca4ab
Miguel Velasco <migue16velasco@gmail.com>
2024-03-30 04:37:01 -0400

    feat: add description posicles

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <div className="w-full bg-white py-16">
+    <div className="w-full bg-white py-16 select-none">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-4xl font-bold mb-8 text-center text-gray-500">
           Paletas
@@ -35,7 +35,7 @@ const Home = () => {
                     {item.nombre_paleta}
                   </h3>
                   <p className="text-gray-700 mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    {item.Paletum.descripcion}
                   </p>
 
                   <p className="text-gray-700 py-2">
@@ -43,7 +43,7 @@ const Home = () => {
                   </p>
                   <div className="py-2 mb-5">
                     <span className="bg-gray-200 rounded-full px-3 text-sm font-semibold text-gray-700 py-3">
-                      Peso {item.peso_unitario} {item.unidad_medida}
+                      Paleta {item.TipoDePaletum.nombre}
                     </span>
                   </div>
 
