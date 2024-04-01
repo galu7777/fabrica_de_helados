5b65e9f70e6804bc14275702809c36356408fa87
Luis Galea <luisgalea31@gmail.com>
2024-04-01 11:32:09 -0400

    feat<api\src\controllers\sale\createsale.js>: add array sales

--- api/src/controllers/Sale/createSale.js
+++ api/src/controllers/Sale/createSale.js
@@ -2,77 +2,81 @@ const { Venta, Cliente, InventarioPaleta, StockPaleta, Paleta } = require('../..
 const response = require('../../utils/response')
 
 module.exports = async (req, res) => {
-    const { id_cliente, id_stock_paleta, cantidad, tasa  } = req.body;
+    const { id_cliente, tasa, ventas } = req.body; // Se espera un array de ventas en el cuerpo de la solicitud
     try {
         const foundClient = await Cliente.findOne({
             where: { id_cliente }
         })
 
         if(foundClient){
-            if(cantidad <= 0){
-                return response(res, 500, {message: 'The quantity must be a positive number and greater than zero.'})
-            }
-            if(tasa <=0){
-                return response(res, 500, {message: 'The rate must be a positive number and greater than zero.'})
-            }
-            const inventoryPopsicle = await InventarioPaleta.findOne({
-                where: { id_inventario_paleta: id_stock_paleta }
-            })
+            for(const venta of ventas) {
+                const { id_stock_paleta, cantidad } = venta;
+                
+                if(cantidad <= 0){
+                    return response(res, 500, {message: 'The quantity must be a positive number and greater than zero.'})
+                }
+                if(tasa <=0){
+                    return response(res, 500, {message: 'The rate must be a positive number and greater than zero.'})
+                }
+                
+                const inventoryPopsicle = await InventarioPaleta.findOne({
+                    where: { id_inventario_paleta: id_stock_paleta }
+                })
 
-            const stockPopsicle = await StockPaleta.findOne({
-                where: { id_stock_paleta },
-                include: [
-                    {
-                        model: Paleta,
-                        attributes: ['id','descripcion','precio']
-                    }
-                ]
-            })
-            if(!inventoryPopsicle && !stockPopsicle){
-                response(res, 400, {message: `No stock or inventory was found with this id: ${id_stock_paleta}`})
-            } else {
-                if(stockPopsicle.cantidad - cantidad <= 0){
-                    return response(res, 400, {message: 'insufficient quantity in inventory.'})
+                const stockPopsicle = await StockPaleta.findOne({
+                    where: { id_stock_paleta },
+                    include: [
+                        {
+                            model: Paleta,
+                            attributes: ['id','descripcion','precio']
+                        }
+                    ]
+                })
+                
+                if(!inventoryPopsicle && !stockPopsicle){
+                    response(res, 400, {message: `No stock or inventory was found with this id: ${id_stock_paleta}`})
                 } else {
-                    const { precio } = stockPopsicle.Paletum
-                    const monto_usd = precio * cantidad
-                    const monto_bs = monto_usd * tasa
-                    // return response(res, 201, {message: 'bien !!!'})
-                    const venta = await Venta.create({
-                        nombre_paleta: inventoryPopsicle.nombre_paleta,
-                        cantidad,
-                        precio,
-                        monto_usd,
-                        monto_bs,
-                        tasa,
-                        ClienteId: id_cliente
-                    })
-                    
-                    const cnt = stockPopsicle.cantidad
-                    await stockPopsicle.update({
-                        cantidad: cnt - cantidad
-                    })
+                    if(stockPopsicle.cantidad - cantidad <= 0){
+                        return response(res, 400, {message: 'insufficient quantity in inventory.'})
+                    } else {
+                        const { precio } = stockPopsicle.Paletum
+                        const monto_usd = precio * cantidad
+                        const monto_bs = monto_usd * tasa
+                        
+                        const newVenta = await Venta.create({
+                            nombre_paleta: inventoryPopsicle.nombre_paleta,
+                            cantidad,
+                            precio,
+                            monto_usd,
+                            monto_bs,
+                            tasa,
+                            ClienteId: id_cliente
+                        })
+                        
+                        const cnt = stockPopsicle.cantidad
+                        await stockPopsicle.update({
+                            cantidad: cnt - cantidad
+                        })
 
-                    await InventarioPaleta.create({
-                        nombre_paleta: inventoryPopsicle.nombre_paleta,
-                        tipo_paleta: inventoryPopsicle.tipo_paleta,
-                        cantidad: -cantidad,
-                        peso_unitario: inventoryPopsicle.peso_unitario,
-                        unidad_medida: inventoryPopsicle.unidad_medida,
-                        tipo: "SALIDA POR VENTA",
-                        PaletumId: inventoryPopsicle.PaletumId,
-                        BatidaDeHeladoId: inventoryPopsicle.BatidaDeHeladoId,
-                        TipoDePaletumId: inventoryPopsicle.TipoDePaletumId
-                    })
-        
-                    return response(res, 201, venta)
+                        await InventarioPaleta.create({
+                            nombre_paleta: inventoryPopsicle.nombre_paleta,
+                            tipo_paleta: inventoryPopsicle.tipo_paleta,
+                            cantidad: -cantidad,
+                            peso_unitario: inventoryPopsicle.peso_unitario,
+                            unidad_medida: inventoryPopsicle.unidad_medida,
+                            tipo: "SALIDA POR VENTA",
+                            PaletumId: inventoryPopsicle.PaletumId,
+                            BatidaDeHeladoId: inventoryPopsicle.BatidaDeHeladoId,
+                            TipoDePaletumId: inventoryPopsicle.TipoDePaletumId
+                        })
+                    }
                 }
             }
+            return response(res, 201, {message: 'Sales successfully recorded.'});
         }
         return response(res, 200, "The client is not registered in the database.")
     } catch (error) {
         console.error('Error: ', error.message)
         return response(res, 500, `Internal Server Error: ${error.message}`)
     }
-
 }
