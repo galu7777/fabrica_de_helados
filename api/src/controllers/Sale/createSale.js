f6a702318c7de28652927451e43541caf42454ba
Luis Galea <luisgalea31@gmail.com>
2024-04-09 15:37:57 -0400

    feat<api\src\controllers\sale\createsale.js>: create registered of clienteventa

--- api/src/controllers/Sale/createSale.js
+++ api/src/controllers/Sale/createSale.js
@@ -1,48 +1,48 @@
-const { Venta, Cliente, InventarioPaleta, StockPaleta, Paleta } = require('../../db')
-const response = require('../../utils/response')
+const { Venta, Cliente, InventarioPaleta, StockPaleta, Paleta, ClienteVenta } = require('../../db');
+const response = require('../../utils/response');
 
 module.exports = async (req, res) => {
-    const { id_cliente, tasa, ventas } = req.body; // Se espera un array de ventas en el cuerpo de la solicitud
+    const { id_cliente, tasa, ventas } = req.body;
     try {
         const foundClient = await Cliente.findOne({
             where: { id_cliente }
-        })
+        });
 
-        if(foundClient){
-            for(const venta of ventas) {
+        if (foundClient) {
+            for (const venta of ventas) {
                 const { id_stock_paleta, cantidad } = venta;
-                
-                if(cantidad <= 0){
-                    return response(res, 500, {message: 'The quantity must be a positive number and greater than zero.'})
+
+                if (cantidad <= 0) {
+                    return response(res, 500, { message: 'The quantity must be a positive number and greater than zero.' });
                 }
-                if(tasa <=0){
-                    return response(res, 500, {message: 'The rate must be a positive number and greater than zero.'})
+                if (tasa <= 0) {
+                    return response(res, 500, { message: 'The rate must be a positive number and greater than zero.' });
                 }
-                
+
                 const inventoryPopsicle = await InventarioPaleta.findOne({
                     where: { id_inventario_paleta: id_stock_paleta }
-                })
+                });
 
                 const stockPopsicle = await StockPaleta.findOne({
                     where: { id_stock_paleta },
                     include: [
                         {
                             model: Paleta,
-                            attributes: ['id','descripcion','precio']
+                            attributes: ['id', 'descripcion', 'precio']
                         }
                     ]
-                })
-                
-                if(!inventoryPopsicle && !stockPopsicle){
-                    response(res, 400, {message: `No stock or inventory was found with this id: ${id_stock_paleta}`})
+                });
+
+                if (!inventoryPopsicle && !stockPopsicle) {
+                    return response(res, 400, { message: `No stock or inventory was found with this id: ${id_stock_paleta}` });
                 } else {
-                    if(stockPopsicle.cantidad - cantidad <= 0){
-                        return response(res, 400, {message: 'insufficient quantity in inventory.'})
+                    if (stockPopsicle.cantidad - cantidad <= 0) {
+                        return response(res, 400, { message: 'insufficient quantity in inventory.' });
                     } else {
-                        const { precio } = stockPopsicle.Paletum
-                        const monto_usd = precio * cantidad
-                        const monto_bs = monto_usd * tasa
-                        
+                        const { precio } = stockPopsicle.Paletum;
+                        const monto_usd = precio * cantidad;
+                        const monto_bs = monto_usd * tasa;
+
                         const newVenta = await Venta.create({
                             nombre_paleta: inventoryPopsicle.nombre_paleta,
                             cantidad,
@@ -50,13 +50,24 @@ module.exports = async (req, res) => {
                             monto_usd,
                             monto_bs,
                             tasa,
-                            ClienteId: id_cliente
+                        });
+                        // Establece la relación entre la venta y el cliente utilizando setCliente
+                        await ClienteVenta.create({
+                            razon_social: foundClient.razon_social,
+                            nombre_paleta: inventoryPopsicle.nombre_paleta,
+                            cantidad,
+                            precio,
+                            monto_usd,
+                            monto_bs,
+                            tasa,
+                            ClienteId: id_cliente,
+                            VentumId: newVenta.id
                         })
-                        
-                        const cnt = stockPopsicle.cantidad
+
+                        const cnt = stockPopsicle.cantidad;
                         await stockPopsicle.update({
                             cantidad: cnt - cantidad
-                        })
+                        });
 
                         await InventarioPaleta.create({
                             nombre_paleta: inventoryPopsicle.nombre_paleta,
@@ -68,15 +79,15 @@ module.exports = async (req, res) => {
                             PaletumId: inventoryPopsicle.PaletumId,
                             BatidaDeHeladoId: inventoryPopsicle.BatidaDeHeladoId,
                             TipoDePaletumId: inventoryPopsicle.TipoDePaletumId
-                        })
+                        });
                     }
                 }
             }
-            return response(res, 201, {message: 'Sales successfully recorded.'});
+            return response(res, 201, { message: 'Sales successfully recorded.' });
         }
-        return response(res, 200, "The client is not registered in the database.")
+        return response(res, 200, "The client is not registered in the database.");
     } catch (error) {
-        console.error('Error: ', error.message)
-        return response(res, 500, `Internal Server Error: ${error.message}`)
+        console.error('Error: ', error.message);
+        return response(res, 500, `Internal Server Error: ${error.message}`);
     }
-}
+};
