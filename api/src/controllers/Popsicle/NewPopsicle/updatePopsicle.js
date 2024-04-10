530933d3707c632e0c9a203336f7317035f26a84
Miguel Velasco <migue16velasco@gmail.com>
2024-04-10 18:53:10 -0400

    feat: update popsicle controller

--- api/src/controllers/Popsicle/NewPopsicle/updatePopsicle.js
+++ api/src/controllers/Popsicle/NewPopsicle/updatePopsicle.js
@@ -1,36 +1,33 @@
-const { Paleta, TipoDePaleta } = require('../../../db');
+const { Paleta } = require('../../../db');
 const response = require('../../../utils/response');
 
 module.exports = async (req, res) => {
-    const { id, nombre, peso, descripcion, id_tipo_de_paleta, precio } = req.body;
+    const { id } = req.params; // Se asume que el ID del elemento a actualizar está en los parámetros de la solicitud
+    const { nombre, peso, precio, descripcion, id_tipo_paleta } = req.body;
 
     try {
-        // Buscar la paleta por su ID
-        const paletaExistente = await Paleta.findByPk(id);
+        const paletaToUpdate = await Paleta.findByPk(id); // Buscar la paleta a actualizar por su ID
 
-        // Verificar si la paleta existe
-        if (!paletaExistente) {
-            return response(res, 404, "Popsicle not found.");
+        if (!paletaToUpdate) {
+            return response(res, 404, "Paleta no encontrada");
         }
 
-        // Verificar si el tipo de paleta existe
-        const tipoDePaleta = await TipoDePaleta.findByPk(id_tipo_de_paleta);
-        if (!tipoDePaleta) {
-            return response(res, 404, "Popsicle type not found.");
+        if (req.file) {
+            const image = req.file.buffer.toString('base64');
+            paletaToUpdate.image = image; // Actualizar la imagen si se proporciona un nuevo archivo
         }
 
-        // Actualizar la paleta con los nuevos datos
-        const updatedPaleta = await paletaExistente.update({
-            nombre,
-            peso,
-            descripcion,
-            tipo_paleta: id_tipo_de_paleta,
-            precio
-        });
+        paletaToUpdate.nombre = nombre;
+        paletaToUpdate.peso = peso;
+        paletaToUpdate.precio = precio;
+        paletaToUpdate.descripcion = descripcion;
+        paletaToUpdate.TipoDePaletumId = id_tipo_paleta;
 
-        return response(res, 200, updatedPaleta);
+        await paletaToUpdate.save(); // Guardar los cambios en la base de datos
+
+        response(res, 200, paletaToUpdate);
     } catch (error) {
-        console.error('Error:', error.message);
+        console.error('Error: ', error.message);
         return response(res, 500, `Internal Server Error: ${error.message}`);
     }
-};
+}
