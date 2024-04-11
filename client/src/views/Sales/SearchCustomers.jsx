3f876c7a7f0d0877452d0c04aca75e8b39330763
Miguel Velasco <migue16velasco@gmail.com>
2024-04-11 11:02:18 -0400

    feat: add button creat customers

--- client/src/views/Sales/SearchCustomers.jsx
+++ client/src/views/Sales/SearchCustomers.jsx
@@ -4,6 +4,8 @@ import TextField from "@mui/material/TextField";
 import { getCustomers } from "../../redux/actions/actions";
 import Autocomplete from "@mui/material/Autocomplete";
 import PropTypes from "prop-types"; // Importar PropTypes
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 const SearchCustomers = ({ onCustomerSelect }) => {
   const dispatch = useDispatch();
@@ -21,57 +23,66 @@ const SearchCustomers = ({ onCustomerSelect }) => {
   };
 
   return (
-    <div className="border-gray-300 border border-solid p-4 rounded-md">
-      <div className="mb-5 flex items-center justify-center ">
-        {data && (
-          <Autocomplete
-            options={data}
-            getOptionLabel={(option) => option.razon_social}
-            renderInput={(params) => (
+    <div>
+      <div className="border-gray-300 border border-solid p-4 rounded-md">
+        <div className="mb-5 flex items-center justify-center ">
+          {data && (
+            <Autocomplete
+              options={data}
+              getOptionLabel={(option) => option.razon_social}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  required
+                  label="Buscar clientes"
+                  variant="outlined"
+                />
+              )}
+              onChange={handleCustomerSelect}
+              sx={{ width: 300 }}
+            />
+          )}
+        </div>
+        <div className="flex justify-center p-4">
+          <Link to="/crear_clientes">
+            <Button variant="outlined" color="primary" >
+              Crear Cliente
+            </Button>
+          </Link>
+        </div>
+        {selectedCustomer && (
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3 flex">
               <TextField
-                {...params}
-                required
-                label="Buscar clientes"
-                variant="outlined"
+                label="Código DNI"
+                value={selectedCustomer.cod_dni}
+                disabled
+                sx={{ my: 1, mr: 2, width: 100 }}
               />
-            )}
-            onChange={handleCustomerSelect}
-            sx={{ width: 300 }}
-          />
+              <TextField
+                label="Cédula/RIF"
+                value={selectedCustomer.cedula_rif}
+                disabled
+                fullWidth
+                sx={{ my: 1, mr: 2, width: "25%" }}
+              />
+              <TextField
+                label="Teléfono"
+                value={selectedCustomer.telefono}
+                disabled
+                fullWidth
+                sx={{ my: 1, mr: 2, width: "25%" }}
+              />
+              <TextField
+                label="Dirección"
+                value={selectedCustomer.direccion}
+                disabled
+                sx={{ my: 1, width: "35%" }}
+              />
+            </div>
+          </div>
         )}
       </div>
-      {selectedCustomer && (
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3 flex">
-            <TextField
-              label="Código DNI"
-              value={selectedCustomer.cod_dni}
-              disabled
-              sx={{ my: 1, mr: 2, width: 100 }}
-            />
-            <TextField
-              label="Cédula/RIF"
-              value={selectedCustomer.cedula_rif}
-              disabled
-              fullWidth
-              sx={{ my: 1, mr: 2, width: "25%" }}
-            />
-            <TextField
-              label="Teléfono"
-              value={selectedCustomer.telefono}
-              disabled
-              fullWidth
-              sx={{ my: 1, mr: 2, width: "25%" }}
-            />
-            <TextField
-              label="Dirección"
-              value={selectedCustomer.direccion}
-              disabled
-              sx={{ my: 1, width: "35%" }}
-            />
-          </div>
-        </div>
-      )}
     </div>
   );
 };
