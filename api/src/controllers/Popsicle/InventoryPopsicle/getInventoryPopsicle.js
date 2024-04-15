0dd9ab9aa6da2d87ca6670bcd016587ed7727590
Miguel Velasco <migue16velasco@gmail.com>
2024-04-15 04:12:23 -0400

    feat: deatil get inventory  popsicle and find all

--- api/src/controllers/Popsicle/InventoryPopsicle/getInventoryPopsicle.js
+++ api/src/controllers/Popsicle/InventoryPopsicle/getInventoryPopsicle.js
@@ -3,10 +3,25 @@ const response = require('../../../utils/response')
 
 module.exports = async (req, res) => {
     try {
-        const inventory = await InventarioPaleta.findAll({
-            include: [TipoDePaleta]
-        })
-        return response(res, 200, inventory)
+        const { id } = req.params;
+        console.log(id)
+
+        if (id) {
+            const inventoryPopsicle = await InventarioPaleta.findByPk(id);
+
+            if (!inventoryPopsicle) {
+                return response(res, 404, 'Inventario no encontrado');
+            }
+            return response(res, 200, inventoryPopsicle)
+
+        } else {
+            const inventory = await InventarioPaleta.findAll({
+                include: [TipoDePaleta]
+            })
+            return response(res, 200, inventory)
+
+        }
+
     } catch (error) {
         return response(res, 500, 'Internal Server Error')
     }
