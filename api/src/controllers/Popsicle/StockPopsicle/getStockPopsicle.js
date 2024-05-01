f969b28f293e1ee5a00e9733503e23dfff2a50f0
Miguel Velasco <migue16velasco@gmail.com>
2024-05-01 04:50:00 -0400

    feat: Filtrar donde la cantidad sea mayor a 0

--- api/src/controllers/Popsicle/StockPopsicle/getStockPopsicle.js
+++ api/src/controllers/Popsicle/StockPopsicle/getStockPopsicle.js
@@ -1,9 +1,12 @@
 const { StockPaleta, Paleta, TipoDePaleta } = require('../../../db')
 const response = require('../../../utils/response')
-
+const { Op } = require('sequelize');
 module.exports = async (req, res) => {
     try {
         const stock = await StockPaleta.findAll({
+            where: {
+                cantidad: { [Op.gt]: 0 } // Filtrar donde la cantidad sea mayor a 0
+            },
             include: [
                 {
                     model: Paleta,
@@ -14,9 +17,9 @@ module.exports = async (req, res) => {
                     attributes: ["id", "nombre"]
                 }
             ]
-        })
-        response(res, 201, stock)
+        });
+        response(res, 201, stock);
     } catch (error) {
-        console.error('Error: ', error.message)
+        console.error('Error: ', error.message);
     }
-}
+};
